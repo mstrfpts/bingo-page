84e8c74a5ace7b1f34affd4e8f8662539653b9ce
derick-dsilva <mstrfpts@gmail.com>
2020-04-02 10:55:52 +0100

    Added note

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import MatrixContainer from "./MatrixContainer";
+import "./MatrixContainer.css";
 
 class App extends Component {
   state = { squares: 100 };
   render() {
-    return <MatrixContainer value={this.state.squares} />;
+    return (
+      <div>
+        <MatrixContainer value={this.state.squares} />
+        <div>
+          <p>
+            Note: Do not refresh. Refreshing the page will reset the numbers
+            generated.
+          </p>
+        </div>
+      </div>
+    );
   }
 }
 
